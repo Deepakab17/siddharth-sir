007f4f30a3cb6b21ef103166a2b92f0779d206c6
Deepakab17 <deepaknab17@gmail.com>
2025-11-24 00:00:20 +0530

    commited firts

---
+++ script.js
@@ -0,0 +1,13 @@
+// function greet(name,age){
+//     console.log("hello", name ,"you are", age, "years old")
+
+// }
+// greet("sharvan",87)
+// let a=10
+// let b=14
+// console.log(a===b)
+let a =5
+let b='6'
+console.log(a+b)
+console.log(typeof(a+b))
+
