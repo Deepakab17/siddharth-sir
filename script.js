b4efbdb2a68be433803e64bb7bd5410ba2917ee5
Deepakab17 <deepaknab17@gmail.com>
2025-11-24 23:56:31 +0530

    javascript conditions

--- script.js
+++ script.js
@@ -6,8 +6,59 @@
 // let a=10
 // let b=14
 // console.log(a===b)
-let a =5
-let b='6'
-console.log(a+b)
-console.log(typeof(a+b))
+// let a =5
+// let b='6'
+// console.log(a+b)
+// console.log(typeof(a+b))
+// console.log(6+1+'7')
+// function sharvan(){
+//     window.alert('Sharvan chor hai')
+
+// }
+// sharvan()
+// function tanishka(){
+//     let user=window.prompt("how are you?")
+//     let user1=window.prompt("how old are you?")
+//     window.alert(user + user1)
+// }
+// tanishka()
+// function positive(){
+//     let num=prompt("pick a number")
+//     if (num<0){
+//         window.alert(-(num))
+//     }
+//     else{
+
+//         window.alert(num)
+//     }
+
+
+// }
+// positive()
+// function factorial(n){
+//     if(n==0){
+//         return 0
+//     }
+//    else{
+//      return n*factorial(n-1)
+//    } 
+// }
+// res=factorial(5)
+// console.log(res)
+
+
+// let obj={
+//     name:"arvind",
+//     age:23,
+//     city:"satna"
+// }
+// console.log(Object.entries(obj))
+// console.log(Object.keys(obj))
+// var a=20
+// var b='6'
+// var c=a-b
+// console.log(c)
+// console.log(typeof(c))
+// console.log(a==b)
+// console.log(a===b)
 
---
+++ script1.js
@@ -0,0 +1,70 @@
+// 1. Ask the user for their age using prompt() and show an alert 'You can vote' if age >= 18 else 'Too
+//  young'.
+// function Vote(){
+//     let age=prompt("enter your age")
+//     if (age>=18){
+//         window.alert("You are eligible to vote")
+    
+//     }
+//     else{
+//         alert("you are not eligible, too young sorry!")
+//     }
+
+// }
+// Vote()
+// 2. Prompt the user for a password and check it against a hardcoded value; alert 'Access granted' or
+// 'Access denied'.
+// function password(){
+//     let pass=prompt("enter your password")
+//     if (pass==1234){
+//         alert("Access Granted")
+//         }
+//         else{
+//             alert("Access Denied")
+//         }
+// }
+// password()
+// 3. Create a grade checker: prompt for marks (0-100) and use if-else-if to alert Grade A/B/C/F.
+
+// function checkGrade() {
+//     let marks = parseInt(prompt("Enter your marks (0-100):"));
+
+//     if (marks >= 90 && marks <= 100) {
+//         alert("Grade A");
+//     }
+//     else if (marks >= 75 && marks < 90) {
+//         alert("Grade B");
+//     }
+//     else if (marks >= 50 && marks < 75) {
+//         alert("Grade C");
+//     }
+//     else if (marks >= 0 && marks < 50) {
+//         alert("Grade F");
+//     }
+//     else {
+//         alert("Invalid marks");
+//     }
+// }
+
+// checkGrade();
+
+
+// 4. Prompt for a number and alert whether it is positive, negative, or zero.
+function checkNumber() {
+    let num = parseInt(prompt("Enter a number:"));
+
+    if (num > 0) {
+        alert("The number is Positive");
+    }
+    else if (num < 0) {
+        alert("The number is Negative");
+    }
+    else if (num === 0) {
+        alert("The number is Zero");
+    }
+    else {
+        alert("Invalid input");
+    }
+}
+
+checkNumber();
